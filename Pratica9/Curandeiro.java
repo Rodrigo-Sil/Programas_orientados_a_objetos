a1872abbeded59ae258ca47a41cc4a0c9af1d611
Rodrigo Silva <Raffaelrodrigo16@gmail.com>
2025-05-26 20:08:09 -0300

    Classes abstrata e concretas

---
+++ Pratica9/Curandeiro.java
@@ -0,0 +1,35 @@
+public class Curandeiro extends Lutador{
+    public Curandeiro(){}
+
+    public Curandeiro(String nome, String alcunha) {
+        setNome(nome);
+        setAlcunha(alcunha);
+    }
+
+    public Curandeiro(String nome, String alcunha, int forca, int velocidade, int vida, int cura) {
+        setNome(nome);
+        setAlcunha(alcunha);
+        setForca(forca);
+        setVelocidade(velocidade);
+        setVida(vida);
+        this.setCura(cura);
+    }
+
+    public void setCura(int cura) {
+        if (cura > getVelocidade() && cura > getVida() && cura > getForca()) {
+            super.setCura(cura);
+        }
+    }
+
+    @Override
+    public int poderDeAtaque() {
+        int a = getCura() + getForca() * getVelocidade();
+        return a;
+    }
+
+    @Override
+    public int poderDeDefesa() {
+        int d = getCura() + getCura() * getVida();
+        return d;
+    }
+}
